3534d881832356d9c5db67c3491a1f6f5745b2e1
Tommy Bolger <tommy.bolger@gmail.com>
2018-09-11 20:20:32 -0700

    Added Vue back into Laravel. Updated bootstrap to use the bootstrap.native instead of jQuery. Updated npm packages.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,18 +1,13 @@
 
 window._ = require('lodash');
-window.Popper = require('popper.js').default;
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * We'll load the Bootstrap native plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
-} catch (e) {}
+require('bootstrap.native/dist/bootstrap-native-v4');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
