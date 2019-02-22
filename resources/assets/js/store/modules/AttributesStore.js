75374393b66c69c9e582553eca696f1b7dbf1be8
Tommy Bolger <tommy.bolger@gmail.com>
2019-02-22 01:19:39 -0800

    #374 Implemented the ability for getSelected to either return the selected record, retrieve the attribute's default record, or fall back to the its first record.
    Made state.selected reactive by resetting it when any value is set.
    
    Fixed various bugs with getFiltered.

--- resources/assets/js/store/modules/AttributesStore.js
+++ resources/assets/js/store/modules/AttributesStore.js
@@ -18,12 +18,13 @@ const AttributesStore = {
         loaded: false,
         records: {},
         records_by_name: {},
-        selected: {},
-        filter_stores: {}
+        default_records: {},
+        filter_stores: {},
+        selected: {}
     },
     mutations: {
         setAll(state, records) {
-            state.names.forEach(function(attribute_name) {
+            state.names.forEach((attribute_name) => {
                 if(records[attribute_name] != null) {
                     state.records[attribute_name] = records[attribute_name];
                     
@@ -33,14 +34,25 @@ const AttributesStore = {
                     
                     state.records_by_name[attribute_name] = {};
                     
-                    records[attribute_name].forEach(function(record) {
+                    records[attribute_name].forEach((record) => {
                         state.records_by_name[attribute_name][record.name] = record;
+                        
+                        if(record['is_default'] != null && record.is_default == 1) {
+                            state.default_records[attribute_name] = record;
+                        }
                     });
                 }
             });
         },
         setSelected(state, payload) {
-            state.selected[payload.attribute] = payload.record;
+            // In order to make state.selected reactive it needs to be cleared and reset from a local variable.
+            let all_selected = state.selected;
+            
+            state.selected = {};
+            
+            all_selected[payload.attribute] = payload.record;
+            
+            state.selected = all_selected;
         },
         setFilterStores(state, payload) {
             state.filter_stores[payload.attribute] = payload.filter_stores;
@@ -53,7 +65,7 @@ const AttributesStore = {
         getAllByNames: (state) => (attribute, names) => {
             let records = [];
             
-            names.forEach(function(name) {
+            names.forEach((name) => {
                 if(state.records_by_name[attribute][name] != null) {
                     records.push(state.records_by_name[attribute][name]);
                 }
@@ -72,42 +84,57 @@ const AttributesStore = {
         },
         getFiltered: (state, getters, root_state, root_getters) => (attribute) => {
             let filtered_names = [];
-            let first_pass_completed = false;
-            
+
             /* 
              * 1. Loop through each filter store
              * 2. Get the selected value of each filter store (if string then grab record by name)
              * 3. Check if filter store record contains the name of the current attribute
-             * 4. If first pass then set the filtered record names from the filter store property.
-             * 5. If not first pass then only set filtered record names if the count is less than the current count.
+             * 4. If filtered_names has not been set then set the filtered record names from the filter store property.
+             * 5. If filtered_names has been set then only set filtered record names if the count is less than the current count.
              */
-            
-            state.filter_stores.forEach(function(filter_store_name) {
+
+            state.filter_stores[attribute].forEach((filter_store_name) => {
                 let filter_option = state.selected[filter_store_name];
-                
+
                 if(typeof filter_option == 'string') {
                     filter_option = state.records_by_name[filter_store_name][filter_option];
                 }
                 else if(filter_option['name'] == null) {
                     filter_option = {};
                 }
-                
-                if(filter_option[filter_property_name] != null) {
-                    if(!first_pass_completed || filter_option[filter_property_name].length < filtered_names.length) {
-                        filtered_names = filter_option[filter_property_name];
-                        
-                        first_pass_completed = true;
+
+                if(filter_option[attribute] != null) {
+                    if(filtered_names.length == 0 || filter_option[attribute].length < filtered_names.length) {
+                        filtered_names = filter_option[attribute];
                     }
                 }
             });
             
-            return getters.getAllByNames(filtered_names);
+            return getters.getAllByNames(attribute, filtered_names);
         },
         getByField: (state) => (attribute, field, value) => {
             return state.records[attribute].find(record => record[field] === value);
         },
         getSelected: state => (attribute) => {
-            return state.selected[attribute];
+            let selected = state.selected[attribute];
+            
+            if(typeof filter_option == 'string' && selected.length == 0) {
+                selected = null;
+            }
+            else if(typeof filter_option == 'object' && filter_option['name'] == null) {
+                selected = null;
+            }
+            
+            if(selected == null) {
+                if(state.default_records[attribute] != null) {
+                    selected = state.default_records[attribute];
+                }
+                else if(state.records[attribute][0] != null) {
+                    selected = state.records[attribute][0];
+                }
+            }
+            
+            return selected;
         }
     },
     actions: {
