e7fe4605f604dc9d4dbfce23d36c1c89c60cc1ee
Tommy Bolger <tommy.bolger@gmail.com>
2019-02-22 02:14:43 -0800

    #377 Updated BasePage to automatically set selected attributes in AttributesStore from route segments.

--- resources/assets/js/components/pages/BasePage.vue
+++ resources/assets/js/components/pages/BasePage.vue
@@ -7,25 +7,46 @@ const BasePage = {
         }
     },
     methods: {
-        loadState() {}
+        loadState() {},
+        initialize(route_parameters) {
+            this.loaded = false;
+        
+            let promise = this.$store.dispatch('attributes/load');
+
+            promise.then(() => {
+                const route_segment_attributes = {
+                    leaderboard_source: 'leaderboard_sources',
+                    leaderboard_type: 'leaderboard_types',
+                    character: 'characters',
+                    release: 'releases',
+                    mode: 'modes',
+                    seeded_type: 'seeded_types',
+                    multiplayer_type: 'multiplayer_types',
+                    soundtrack: 'soundtracks'
+                };
+                
+                Object.keys(route_segment_attributes).forEach((route_segment) => {
+                    let route_segment_value = null;
+                    
+                    if(route_parameters[route_segment] != null) {
+                        route_segment_value = route_parameters[route_segment];
+                    }
+
+                    this.$store.commit('attributes/setSelected', {
+                        attribute: route_segment_attributes[route_segment],
+                        record: route_segment_value
+                    });
+                });
+                
+                this.loadState(route_parameters);
+            });
+        }
     },
     created() {
-        this.loaded = false;
-        
-        let promise = this.$store.dispatch('attributes/load');
-
-        promise.then(() => {
-            this.loadState(this.$route.params);
-        });
+        this.initialize(this.$route.params);
     },
     beforeRouteUpdate(to, from, next) {
-        this.loaded = false;
-
-        let promise = this.$store.dispatch('attributes/load');
-        
-        promise.then(() => {
-            this.loadState(to.params);
-        });
+        this.initialize(to.params);
         
         next();
     }
