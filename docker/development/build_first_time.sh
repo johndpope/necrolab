363e4d3e5930298d7b8995b137b7f5ce57b314b2
Tommy Bolger <tommy.bolger@gmail.com>
2019-03-29 10:25:53 -0700

    #397 Added npm to be installed with the app docker service. Added the service name to the node watcher service in the docker composer. Added more build steps to the script that build the Laravel environment for the first time.

--- docker/development/build_first_time.sh
+++ docker/development/build_first_time.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-cd /var/www
+cd ../../
 
-if [ ! -f .env ]
+if [ ! -f ./.env ]
     printf "Copying .env.example to .env\n"
-    cp .env.example .env
+    cp ./.env.example ./.env
     
-    printf "Generating Laravel key\n"
+    printf "Generating Laravel key.\n"
     php artisan key:generate
     
-    printf "Running migrations\n"
+    printf "Running migrations.\n"
     php artisan migrate
     
-    printf "Running seeders\n"
+    printf "Running seeders.\n"
     php artisan db:seed
+    
+    printf "Creating public storage."
+    php artisan storage:link
+    
+    printf "Generating attributes json.\n"
+    php artisan attributes:generate_json
+    
+    printf "Done!\n"
 fi
