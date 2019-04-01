6c4fa75269a88ae1b09421e79687d9c48afb9f06
Tommy Bolger <tommy.bolger@gmail.com>
2019-04-01 10:29:33 -0700

    #399 Added process to clear opcache.

---
+++ app/Jobs/Cache/ClearOpCache.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs\Cache;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
+
+class ClearOpCache implements ShouldQueue {
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 1;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct() {}
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle() {
+        Cache::store('opcache')->clear();
+    }
+}
