2897faf9268adf7cd0a895f29ae5e46fdd59bdbe
Tommy Bolger <tommy.bolger@gmail.com>
2019-04-29 13:13:09 -0700

    #408 Added Laravel optimization commands for both the first time install script and the update script in the production environment.

--- docker/environments/production/first_time_app_install.sh
+++ docker/environments/production/first_time_app_install.sh
@@ -50,6 +50,9 @@ then
     printf "========== Clearing cache and opcache. ==========\n"
     php artisan cache:opcache:clear
     php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
+    php artisan view:clear
 
     printf "========== Creating Steam schema. ==========\n"
     php artisan leaderboards:sources:create_schema --leaderboard_source=steam
@@ -62,6 +65,18 @@ then
 
     printf "========== Generating attributes json. ==========\n"
     php artisan attributes:generate_json
+    
+    printf "========== Caching the config. ==========\n"
+    php artisan config:cache
+    
+    printf "========== Caching routes. ==========\n"
+    php artisan route:cache
+    
+    printf "========== Optimizing the classmap. ==========\n"
+    php artisan optimize --force
+    
+    printf "========== Optimizing Composer. ==========\n"    
+    composer dump-autoload -o
 
     printf "========== Running npm. ==========\n"
     npm run production
--- docker/environments/production/update.sh
+++ docker/environments/production/update.sh
@@ -17,9 +17,24 @@ then
     printf "========== Clearing cache and opcache. ==========\n"
     php artisan cache:opcache:clear
     php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
+    php artisan view:clear
     
     printf "========== Generating attributes json. ==========\n"
     php artisan attributes:generate_json
+    
+    printf "========== Caching the config. ==========\n"
+    php artisan config:cache
+    
+    printf "========== Caching routes. ==========\n"
+    php artisan route:cache
+    
+    printf "========== Optimizing the classmap. ==========\n"
+    php artisan optimize --force
+    
+    printf "========== Optimizing Composer. ==========\n"    
+    composer dump-autoload -o
     
     printf "========== Running npm. ==========\n"
     npm run production
