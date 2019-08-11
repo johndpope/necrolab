f0c87e159fa2abf1d98ccef98fe66fdbf676d881
Tommy Bolger <tommy.bolger@gmail.com>
2019-08-10 21:31:01 -0700

    #459 Added commands to install passport and generate its keys into the first time install scripts for development and production.

--- docker/environments/development/first_time_app_install.sh
+++ docker/environments/development/first_time_app_install.sh
@@ -4,40 +4,46 @@ if [ ! -f ./.env ]
 then
     printf "========== Copying .env.example to .env. ==========\n"
     cp ./.env.example ./.env
-    
+
     printf "========== Running composer install. ==========\n"
     composer install
-    
+
     printf "========== Running npm install. ==========\n"
     npm install
-    
+
     printf "========== Generating Laravel key. ==========\n"
     php artisan key:generate
-    
+
     printf "========== Running migrations. ==========\n"
     php artisan migrate
-    
+
+    printf "========== Install Passport. ==========\n"
+    php artisan passport:install
+
+    printf "========== Generate Passport Keys. ==========\n"
+    php artisan passport:keys
+
     printf "========== Running seeders. ==========\n"
     php artisan db:seed
-    
+
     printf "========== Clearing cache and opcache. ==========\n"
     php artisan cache:opcache:clear
     php artisan cache:clear
-    
+
     printf "========== Creating Steam schema. ==========\n"
     php artisan leaderboards:sources:create_schema --leaderboard_source=steam
-    
+
     printf "========== Generating dates. ==========\n"
     php artisan dates:add_range --start_date=2014-07-01
-    
+
     printf "========== Creating public storage. ==========\n"
     php artisan storage:link
-    
+
     printf "========== Generating attributes json. ==========\n"
     php artisan attributes:generate_json
-    
+
     printf "========== Running npm. ==========\n"
     npm run dev
-    
+
     printf printf "========== All done! You can now navigate to https://local.necrolab.com ==========\n"
 fi
--- docker/environments/production/first_time_app_install.sh
+++ docker/environments/production/first_time_app_install.sh
@@ -4,11 +4,11 @@ if [ ! -f ./.env ]
 then
     printf "========== Copying .env.example to .env. ==========\n"
     cp ./.env.example ./.env
-    
+
     echo "========== Set Database Credentials =========="
-    
+
     read -p 'Database Username: ' database_username
-    
+
     while true; do
         read -sp 'Database Password: ' database_password
         echo
@@ -16,22 +16,22 @@ then
         echo
         [ "$database_password" = "$database_confirm_password" ] && break || echo "Password is incorrect. Please try again."
     done
-    
+
     sed -i "s/DB_USERNAME\=.*/DB_USERNAME=$database_username/g" ./.env
-    
+
     sed -i "s/DB_PASSWORD\=.*/DB_PASSWORD=$database_password/g" ./.env
-    
-    
+
+
     sed -i "s/APP_ENV\=.*/APP_ENV=production/g" ./.env
-    
+
     sed -i "s/APP_DEBUG\=.*/APP_DEBUG=false/g" ./.env
-    
+
     sed -i "s/APP_URL\=.*/APP_URL=https\:\/\/beta\.necrolab\.com/g" ./.env
-    
+
     STEAM_REDIRECT_URI=https://local.necrolab.com/login/steam/success
-    
+
     sed -i "s/STEAM_REDIRECT_URI\=.*/STEAM_REDIRECT_URI=https\:\/\/beta\.necrolab\.com\/login\/steam\/success/g" ./.env
-   
+
     printf "========== Running composer install. ==========\n"
     composer install
 
@@ -43,6 +43,12 @@ then
 
     printf "========== Running migrations. ==========\n"
     php artisan migrate
+
+    printf "========== Install Passport. ==========\n"
+    php artisan passport:install
+
+    printf "========== Generate Passport Keys. ==========\n"
+    php artisan passport:keys
 
     printf "========== Running seeders. ==========\n"
     php artisan db:seed
@@ -65,14 +71,14 @@ then
 
     printf "========== Generating attributes json. ==========\n"
     php artisan attributes:generate_json
-    
+
     printf "========== Caching the config. ==========\n"
     php artisan config:cache
-    
+
     printf "========== Caching routes. ==========\n"
     php artisan route:cache
-    
-    printf "========== Optimizing Composer. ==========\n"    
+
+    printf "========== Optimizing Composer. ==========\n"
     composer dump-autoload -o
 
     printf "========== Running npm. ==========\n"
